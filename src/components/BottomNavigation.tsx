561111a2c281bd3ff90ef509a3e577a68edc2cca
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-08-25 23:54:02 +0300

    implement Header, Hero, SearchBar, BottomNavigation components

---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,19 @@
+import { Bookmark, Home, PlusCircle } from "lucide-react";
+
+const BottomNavigation = () => {
+  return (
+    <div className="fixed bottom-0 flex w-full justify-around border-t border-slate-200 p-4 shadow-xl">
+      <button className="rounded-full bg-yellow-300 p-4">
+        <Home></Home>
+      </button>
+      <button>
+        <PlusCircle color="gray"></PlusCircle>
+      </button>
+      <button>
+        <Bookmark color="gray"></Bookmark>
+      </button>
+    </div>
+  );
+};
+
+export default BottomNavigation;
