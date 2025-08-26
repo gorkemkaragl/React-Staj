d9ddf3004cf7df0a71ed6e0bc0f593d157e765a3
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-08-26 16:23:01 +0300

    add PopularRecipes, Categories, RecipeCard and minor fixes

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -2,7 +2,7 @@ import { Bookmark, Home, PlusCircle } from "lucide-react";
 
 const BottomNavigation = () => {
   return (
-    <div className="fixed bottom-0 flex w-full justify-around border-t border-slate-200 p-4 shadow-xl">
+    <div className="fixed bottom-0 flex w-full justify-around border-t bg-white border-slate-200 p-4 shadow-xl">
       <button className="rounded-full bg-yellow-300 p-4">
         <Home></Home>
       </button>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ const SearchBar = () => {
   return (
     <div className="px-4">
       <div className="relative flex items-center gap-4 rounded-full bg-gray-200 p-2 px-6">
-        <Search size={20}></Search>
+        <Search size={24}></Search>
         <input className="flex-1 outline-0" type="text" placeholder="Search any recipe" />
         <div className="rounded-xl bg-white p-2 shadow-xl">
           <Settings2 size={20}></Settings2>
