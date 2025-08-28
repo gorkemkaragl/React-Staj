155b0d0c50fac3cd9ad78d2eb16d6503b4d5d8c4
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-08-28 17:42:19 +0300

    Add main application components: Navigation, Categories, Products, BottomNavigation, SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import BottomNavigation from "./components/BottomNavigation";
+import Categories from "./components/Categories";
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return <div className="grid gap-4">
+    <Navigation></Navigation>
+    <Categories></Categories>
+    <Products></Products>
+    <BottomNavigation></BottomNavigation>
+  </div>;
 };
 
 export default App;
---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,8 @@
+
+const BottomNavigation = () => {
+  return (
+    <div className="fixed bottom-0">BottomNavigation</div>
+  )
+}
+
+export default BottomNavigation
