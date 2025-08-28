36a731d01f03808853e823cfd92928a1843d801d
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-08-28 22:45:44 +0300

    Refactor App layout and implement BottomNavigation component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import BottomNavigation from "./components/BottomNavigation";
 import Categories from "./components/Categories";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
-  return <div className="grid gap-4">
+  return <div className="min-h-screen bg-background max-w-md mx-auto relative">
     <Navigation></Navigation>
+    <SearchBar></SearchBar>
     <Categories></Categories>
     <Products></Products>
     <BottomNavigation></BottomNavigation>
