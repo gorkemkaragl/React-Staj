12629c9b07111128d2ad1dadebb3bc00a6ced1ea
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-09-03 00:44:47 +0300

    Add Hero, HeroImageContainer, and HeroTextContainer components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,18 @@
+import HeroImageContainer from "./HeroImageContainer"
+import HeroTextContainer from "./HeroTextContainer"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative bg-gradient-to-br from-orange-50 to-teal-50 py-20">
+      {/* Padding ve max width ile ilgili işlemler */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <HeroTextContainer></HeroTextContainer>
+          <HeroImageContainer></HeroImageContainer>
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/HeroImageContainer.tsx
@@ -0,0 +1,25 @@
+import { Heart } from "lucide-react";
+
+const HeroImageContainer = () => {
+  return (
+    <div className="relative">
+      <img
+        src="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=800"
+        alt="Papağan"
+        className="rounded-2xl shadow-2xl"
+      />
+
+      <div className="absolute -bottom-6 -left-6 bg-white p-4 rounded-xl shadow-xl">
+        <div className="flex items-center gap-3">
+          <Heart className="text-orange-600"></Heart>
+          <div>
+            <p className="font-semibold">Uzman Bakım</p>
+            <p className="text-sm text-gray-600">7+ yıl deneyim</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImageContainer;
