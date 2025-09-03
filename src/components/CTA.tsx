92cd54d6164fd3ebc1e2004db0440c7cb3e8595b
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-09-03 18:30:14 +0300

    Add CTA and Footer components with contact and social media information

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,8 +1,36 @@
+import { Phone } from "lucide-react";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="bg-orange-600 py-16">
+      <div className="space-y-6 max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <h2 className="text-3xl font-bold text-white">
+          Kuşunuzun sağlığı için hemen iletişime geçin
+        </h2>
+        <p className="text-xl text-orange-100">
+          Deneyimli veteriner hekiminizden randevu alın
+        </p>
+        {/* Button */}
+        <div className="flex flex-col sm:flex-row gap-4 justify-center">
+          <a
+            href=""
+            className="flex items-center bg-white text-orange-600 px-8 py-3 rounded-lg font-semibold justify-center gap-4
+          hover:bg-orange-50 transition-colors"
+          >
+            <Phone className="h-5 w-5"></Phone>
+            <span>+90(500) 661 61 61</span>
+          </a>
+          <a
+            href=""
+            className="flex items-center bg-orange-700 text-white px-8 py-3 rounded-lg font-semibold 
+          hover:bg-orange-800 transition-colors"
+          >
+            <span>Iletişim bilgileri</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
